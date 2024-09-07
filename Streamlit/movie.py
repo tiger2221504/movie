d38f2879fab2ac4c7a1664a637cd2f4321734ee5
tora <96847677+tiger2221504@users.noreply.github.com>
2024-09-07 23:24:41 +0900

    concatenate_audioclipsのimport

--- Streamlit/movie.py
+++ Streamlit/movie.py
@@ -1,7 +1,7 @@
 import os
 import streamlit as st
 from moviepy.editor import VideoFileClip, concatenate_videoclips, AudioFileClip, CompositeAudioClip, ImageClip
-from moviepy.audio.AudioClip import AudioArrayClip
+from moviepy.audio.AudioClip import AudioArrayClip, concatenate_audioclips
 import tempfile
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
